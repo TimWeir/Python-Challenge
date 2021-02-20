6ffb274610fbcfe6c30a2d1b2cfe862216a8cbb9
Tim Weir <timothy.weir@comcast.net>
2021-02-20 14:15:46 -0500

    Homework 3 submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,75 @@
-PyBank
+#PyBank
+import os
+import csv
 
+#Fix file path for source file
+budget_file = os.path.join("Resources", "budget_data.csv")
+#Call the file to write results
+output_file = os.path.join("Analysis", "PyBank_results.txt")
+
+#define lists
+months = []
+amounts = []
+change = []
+curr_amt = 0
+last_amt = 0
+row_count = 0
+total_amount = 0
+
+#Open source file as read only
+with open(budget_file, newline='') as csv_source:
+    csvreader = csv.reader(csv_source, delimiter=",")
+    # Trick to ignore the header row as it is not involved in the analysis
+    csv_header = next(csv_source)
+
+    #for amounts in csv_source:
+    for row in csvreader:
+        
+        #add months to list
+        months.append(row[0])
+        
+        #add profit/loss amounts to list
+        curr_amt = row[1]
+        amounts.append(curr_amt)
+
+        #total amount for output
+        total_amount = total_amount + int(row[1])
+
+        #find change with exception for 0 condition on first instance
+        #"if" statement handles first condition
+        if row_count == 0:
+            change.append(0)
+            row_count = row_count + 1
+            last_amt = curr_amt
+        #"else" statment handles all other conditions
+        else:
+            difference = int(curr_amt) - int(last_amt)
+            change.append(difference)
+            row_count = row_count + 1
+            last_amt = curr_amt
+
+    # OUTPUT
+    # Header of the analysis
+    print("") #bumpity bump
+    print(f"Financial Analysis by Tim")
+    print(f"----------------------------")
+    # Print the months
+    print(f"Total months: {row_count}")
+    # Print the sum of all amounts
+    print(f"Total amount: ${total_amount}")
+    #print other metrics
+    print(f"Average Change: ${round(sum(change)/(len(change)-1),2)}")
+    print(f"Greatest Increase in Profits: {months[change.index(max(change))]} (${max(change)})")
+    print(f"Greatest Decrease in Profits: {months[change.index(min(change))]} (${min(change)})")
+    print("") #bump for next row spacer
+
+#write file for results
+with open(output_file, 'w') as txtwriter:
+    #write results
+    txtwriter.write(f"Financial Analysis by Tim\n")
+    txtwriter.write(f"----------------------------\n")
+    txtwriter.write(f"Total months: {row_count}\n")
+    txtwriter.write(f"Total amount: ${total_amount}\n")
+    txtwriter.write(f"Average Change: ${round(sum(change)/(len(change)-1),2)}\n")
+    txtwriter.write(f"Greatest Increase in Profits: {months[change.index(max(change))]} (${max(change)})\n")
+    txtwriter.write(f"Greatest Decrease in Profits: {months[change.index(min(change))]} (${min(change)})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,71 @@
-PyPoll
+#PyPoll
+import os
+import csv
 
+#Fix file path for source file
+source_file = os.path.join("Resources", "election_data.csv")
+#Call the file to write results
+output_file = os.path.join("Analysis", "PyPoll_results.txt")
+
+#define objects
+candidates = []
+votes = []
+total_votes = 0
+margins = []
+most_votes = 0
+
+#Open source file as read only
+with open(source_file, 'r', newline='') as csv_source:
+    csvreader = csv.reader(csv_source, delimiter=",")
+    # Trick to ignore the header row as it is not involved in the analysis
+    csv_header = next(csv_source)
+
+    #for loop in csv_source:
+    for row in csvreader:
+        #total votes cast
+        total_votes = total_votes + 1
+
+        #name of the candidate to analyze
+        name = row[2]
+   
+        if name in candidates: #if the name is in the candidates list
+            name_index = candidates.index(name) #grab the index number
+            votes[name_index] = votes[name_index] + 1 #and increment the number of votes (i.e. count) for this name
+        else:# if the name is not in the list
+            candidates.append(name) # add the new name
+            votes.append(1) #and add the first vote for the new name
+    
+    # do the numbers
+    for cand_count in range(len(candidates)): #for each name in the candidates list (by index)
+        percent = votes[cand_count]/total_votes*100 #calc the name's percentage of total votes
+        margins.append(percent) #add the percentage of total votes to list called margins
+        #store the index of name with most votes (this is the winner)
+        if votes[cand_count] > most_votes: #if the votes at this index in the votes list is greater than "most_votes"
+            most_votes = votes[cand_count] #replace "most_votes"
+            windex = cand_count #and the candidate name at this index is the winner or windex (see what I did there?)
+    winner = candidates[windex] #name the winner by index in the list of candidates    
+
+    # OUTPUT
+    print("") #bumpity bump
+    print(f"Election Results")
+    print(f"----------------------------")
+    print(f"Total votes: {total_votes}")
+    print(f"----------------------------")
+    for cand_count in range(len(candidates)):
+        print(f"{candidates[cand_count]}: {margins[cand_count]:.3f}% ({votes[cand_count]})")
+    print(f"----------------------------")
+    print(f"Winner: {candidates[windex]}")
+    print(f"----------------------------")
+
+#file for results
+with open(output_file, 'w') as txtwriter:
+    #write results
+    txtwriter.write(f"Election Results\n")
+    txtwriter.write(f"----------------------------\n")
+    txtwriter.write(f"Total votes: {total_votes}\n")
+    txtwriter.write(f"----------------------------\n")
+    for cand_count in range(len(candidates)):
+        txtwriter.write(f"{candidates[cand_count]}: {margins[cand_count]:.3f}% ({votes[cand_count]})\n")
+    txtwriter.write(f"----------------------------\n")
+    txtwriter.write(f"Winner: {candidates[windex]}\n")
+    txtwriter.write(f"----------------------------\n")
